3110e2de59377741af52cb00f24a6dd05bc53da4
BlackLion1313 <mila.alenina@gmail.com>
2023-03-06 18:06:28 +0100

    Initial commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h2>Welcome to this EPhonebook App</h2>
+      <h2>To see the contacts list click the link Contacts</h2>
+    </div>
+  );
+}
+
+export default Home;
